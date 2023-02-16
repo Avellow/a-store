b260b6b622ff1615bb38e6ab569b7ace1e78a6bd
Avellow <avellow@yandex.ru>
2023-02-16 12:31:36 +0300

    modify: добавить сабтайтл для карточек при наличии

--- src/components/CardsContainer/CardsContainer.tsx
+++ src/components/CardsContainer/CardsContainer.tsx
@@ -4,17 +4,17 @@ import styles from './CardsContainer.module.css';
 import { ProductCard } from '../ProductCard/ProductCard';
 
 export const CardsContainer = ({ className, cards, ...restProps }: CardsContainerProps): JSX.Element => {
-    return (
-        <div className={cn(className, styles.container)} {...restProps}>
-            <ul className={styles.list}>
-                {
-                    cards.map(card => (
-                        <li key={card.id} className={styles.item}>
-                            <ProductCard {...card} />
-                        </li>
-                    ))
-                }
-            </ul>
-        </div>
-    );
+  return (
+    <div className={cn(className, styles.container)} {...restProps}>
+      <ul className={styles.list}>
+        {
+          cards.map(card => (
+            <li key={card.id} className={styles.item}>
+              <ProductCard {...card} />
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
 };
--- src/components/Group/Group.tsx
+++ src/components/Group/Group.tsx
@@ -7,27 +7,27 @@ import { makeLineBreaks } from '../../vendor/constants';
 
 export const Group = ({ group, className, ...restProps }: GroupProps): JSX.Element => {
 
-    const { title, id, description, products } = group;
+  const { title, description, products } = group;
 
-    return (
-        <div className={cn(className, styles.group)} {...restProps}>
-            <Typography.TitleResponsive
-                tag='h3'
-                weight='bold'
-                view='xlarge'
-                className={styles.title}
-            >
-                {makeLineBreaks(title)}
-            </Typography.TitleResponsive>
-            <Typography.Text
-                tag='p'
-                weight='bold'
-                view='primary-large'
-                className={styles.subtitle}
-            >
-                {description}
-            </Typography.Text>
-            <CardsContainer cards={products} />
-        </div>
-    )
-}
+  return (
+    <div className={cn(className, styles.group)} {...restProps}>
+      <Typography.TitleResponsive
+        tag='h3'
+        weight='bold'
+        view='xlarge'
+        className={styles.title}
+      >
+        {makeLineBreaks(title)}
+      </Typography.TitleResponsive>
+      <Typography.Text
+        tag='p'
+        weight='bold'
+        view='primary-large'
+        className={styles.subtitle}
+      >
+        {description}
+      </Typography.Text>
+      <CardsContainer cards={products} />
+    </div>
+  );
+};
