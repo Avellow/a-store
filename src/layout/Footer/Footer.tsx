8750101445c4901ab52066af3f9d3d8362e68824
Avellow <avellow@yandex.ru>
2023-03-07 12:13:25 +0300

    modify: рефакторинг стилей футера

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -5,11 +5,11 @@ import { format } from 'date-fns';
 import { Typography } from '@alfalab/core-components/typography';
 
 export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
-    return (
-        <footer className={cn(className, styles.footer)}>
-            <Typography.Text className={styles.rights} weight='medium' view='primary-small'>
-                &copy; ООО "Альфа Фьюче Пипл", {format(new Date(), 'yyyy')}
-            </Typography.Text>
-        </footer>
-    );
+  return (
+    <footer className={cn(className, styles.footer)}>
+      <Typography.Text className={styles.rights}>
+        &copy; ООО "Альфа Фьюче Пипл", {format(new Date(), 'yyyy')}
+      </Typography.Text>
+    </footer>
+  );
 };
